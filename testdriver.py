36d062f986b1de6592ba6c4a45a5cd0b7b073d45
zhillan Attarizal-R <zhilanar@gmail.com>
2023-02-17 16:53:22 +0700

    temp

--- testdriver.py
+++ testdriver.py
@@ -3,11 +3,8 @@ import ciphers.tools as tools
 import ciphers.extendedVigenere as ev
 import ciphers.playfair as pf
 
-plaintext = "Where no counsel is, the people fall... (Proverbs 11:14)"
-key = "11073"
-
-plain1 = "V2hlcmUgbm8gY291bnNlbCBpcywgdGhlIHBlb3BsZSBmYWxsLi4uIChQcm92ZXJicyAxMToxNCk="
-
+plaintext = "Where no counsel is, the people fall... but in the multitude of counsellors there is safety."
+key = "Proverbs 11:14"
 
 cipherstream = mosc.mosc_encrypt(plaintext, key)
 print(cipherstream, "\n\n")
